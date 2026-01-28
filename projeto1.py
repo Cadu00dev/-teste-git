7e39df8f80c291fd27dd848aee79310a896fdbfc
Carlos Eduardo <cesr00904@gmail.com>
2026-01-28 19:46:58 -0300

    primeiro commit

---
+++ projeto1.py
@@ -0,0 +1,7 @@
+print("Gerador de nome de Banda.")
+
+cidade = input("Em qual cidade voce mora?\n")
+
+animal = input("Qual e o nome do seu animal de estimação?\n")
+
+print("O nome da sua banda gerado sera:", cidade , animal)
